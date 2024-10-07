226560959f8574ec0b0eae59a156e1ca578b3775
Kathy <kathleen.jvrp@gmail.com>
2024-10-07 23:08:51 +0800

    added modules

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html')); // Serve index.html
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
