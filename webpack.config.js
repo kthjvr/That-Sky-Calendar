26ec0a9ce9ad5630d5f2ad6190918be0ca27e820
Kathy <kathleen.jvrp@gmail.com>
2024-10-09 14:46:28 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { watch } = require('fs');
 
 module.exports = {
-  mode: 'production', 
+  mode: 'development', 
   entry: './src/index.js', 
   output: {
     path: path.resolve(__dirname, 'dist'),
