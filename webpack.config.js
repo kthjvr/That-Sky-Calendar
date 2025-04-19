14d67b3e497c12b4adc4c956468ba717cb809ade
Kathy <kathleen.jvrp@gmail.com>
2025-04-19 13:00:55 +0800

    archive unnecessary files

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development', 
+  mode: 'development',
   entry: {
-    index: './src/index.js', 
-    event: './src/event.js', 
-    spirits: './src/spirits.js'
+    index: './src/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js', 
+    filename: '[name].bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html', 
-      template: './src/index.html', 
+      filename: 'index.html',
+      template: './src/index.html',
       chunks: ['index']
     }),
-    new HtmlWebpackPlugin({
-      filename: 'event.html', 
-      template: './src/event.html', 
-      chunks: ['event']
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'spirits.html', 
-      template: './src/spirits.html', 
-      chunks: ['spirits']
-    })
   ],
   watch: false
 };
