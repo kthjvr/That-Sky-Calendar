bd4022c26a61be984d90acdd034c4c2d2be41872
Kathy <kathleen.jvrp@gmail.com>
2025-09-02 22:09:59 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
   mode: 'production',
@@ -15,6 +18,17 @@ module.exports = {
       filename: 'index.html',
       template: './src/index.html',
       chunks: ['index']
+    }),
+
+    // ✅ Make env variables available in your code
+    new webpack.DefinePlugin({
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+      'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
     }),
   ],
   optimization: {
